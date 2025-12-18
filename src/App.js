b5822b29df7aef11435e6692113110faa868d459
Vishnutt <vishnutt@xminds.com>
2025-12-18 17:14:47 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+
+function App() {
+  return (
+    <div className="App">
+    <HomeScreen/>
+    </div>
+  );
+}
+
+export default App;
