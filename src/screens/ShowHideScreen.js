45518c2a5926afbe0d3834c83866b0b546de4d5f
Vishnutt <vishnutt@xminds.com>
2025-12-19 13:02:55 +0530

    skill list using string array

--- src/screens/ShowHideScreen.js
+++ src/screens/ShowHideScreen.js
@@ -6,9 +6,13 @@ import catImage from "../assets/cat.jpg";
 function ShowHideScreen() {
   const [name, setName] = useState("");
   const [hideshow, setHideShow] = useState(false);
+  const [skills, setSkills] = useState([]);
+  const [skillsEntered, setSkillsEntered] = useState("");
+  console.log("Skills:", skills);
   return (
     <div style={{ padding: "20px" }}>
       {/* text box on entering your name */}
+      <p>1st Question</p>
       <input
         type="text"
         placeholder="Enter your name"
@@ -19,8 +23,33 @@ function ShowHideScreen() {
       <p>{name && `Welcome ${name}`}</p>
 
       {/* hide/show image */}
-
+      <p>2nd Question</p>
       {hideshow && <img src={catImage} alt="Profile" width="200" />}
+
+      {/* skill list using string array */}
+      <p>2nd Question</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your skill here"
+          value={skillsEntered}
+          onChange={(event) => setSkillsEntered(event.target.value)}
+        />
+        <button
+          onClick={() => {
+            setSkills([...skills, skillsEntered]);
+            setSkillsEntered("");
+          }}
+        >
+          Add Skill
+        </button>
+
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
