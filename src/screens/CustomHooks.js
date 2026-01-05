af6be8c2840594ed97a50a0bdb0069b9a7570486
Vishnutt <vishnutt@xminds.com>
2026-01-05 13:28:41 +0530

    useHook useForm example

--- src/screens/CustomHooks.js
+++ src/screens/CustomHooks.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import useCounter from "../hooks/useCounter";
 import DataDisplayComponent from "../hooks/DataDisplayComponent";
+import useForm from "../hooks/useForm";
 
 const CustomHooks = () => {
   const [count, increment] = useCounter(0);
   const endUrl = `/posts`;
+
+  const name = useForm("");
+  const age = useForm("");
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name.value}, Age: ${age.value}`);
+    setSubmittedData({
+      name: name.value,
+      age: age.value,
+    });
+  };
 
   return (
     <div>
@@ -14,7 +28,27 @@ const CustomHooks = () => {
       <br />
       <br />
 
-      <DataDisplayComponent url={endUrl} />
+      {/* <DataDisplayComponent url={endUrl} /> */}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Name:
+            {/* The spread operator here is equivalent to value={name.value} onChange={name.onChange} */}
+            <input type="text" {...name} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Age:
+            <input type="number" {...age} />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      <p>Entered Name: {submittedData?.name}</p>
+      <p>Entered Age: {submittedData?.age}</p>
     </div>
   );
 };
